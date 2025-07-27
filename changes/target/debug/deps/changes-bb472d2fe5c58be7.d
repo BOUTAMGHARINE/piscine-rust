cc85d0f490a8cb78ac105d4d81c3c22cb736c53b
youssefhrouk <yhrouk@gmail.com>
2025-07-27 16:19:20 +0100

    hgjh

---
+++ changes/target/debug/deps/changes-bb472d2fe5c58be7.d
@@ -0,0 +1,5 @@
+/home/yhrouk/piscine-rust/changes/target/debug/deps/changes-bb472d2fe5c58be7.d: src/lib.rs
+
+/home/yhrouk/piscine-rust/changes/target/debug/deps/libchanges-bb472d2fe5c58be7.rmeta: src/lib.rs
+
+src/lib.rs:
